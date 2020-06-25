45f4831e8366a468731d02d01c1bcca1dc8b1a65
jeeweb <52396673+jeeweb@users.noreply.github.com>
2020-06-25 17:09:54 +0900

    prompt 문구 수정 (제시어 노출)

--- word-chain.js
+++ word-chain.js
@@ -1,13 +1,13 @@
 //끝말잇기
 
-let word = '단어';
+let word = "자동차";
 document.write(word);
 while (true) {
-    const newWord = prompt('다음 단어를 입력하세요')
-    if (newWord[0] === word[word.length - 1]) {
-        word = newWord
-        document.write('-' + word)
-    } else {
-        alert('틀렸습니다!')
-    }
+  const newWord = prompt("<" + word + "> -> 다음 단어를 입력하세요");
+  if (newWord[0] === word[word.length - 1]) {
+    word = newWord;
+    document.write("-" + word);
+  } else {
+    alert("다시 입력해주세요!");
+  }
 }
